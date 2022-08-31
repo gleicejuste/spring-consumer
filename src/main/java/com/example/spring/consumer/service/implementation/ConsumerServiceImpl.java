85ba7d327a4e309753d857e58242a6f1b2a9ec85
Gleice Juste <glejuste@hotmail.com>
2022-08-31 16:38:19 -0300

    Initial commit

---
+++ src/main/java/com/example/spring/consumer/service/implementation/ConsumerServiceImpl.java
@@ -0,0 +1,16 @@
+package com.example.spring.consumer.service.implementation;
+
+import com.example.spring.consumer.dto.Message;
+import com.example.spring.consumer.service.ConsumerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerServiceImpl implements ConsumerService {
+
+    @Override
+    public void action(Message message) {
+        System.out.println("##############################################################################################");
+        System.out.println(message.getText());
+    }
+
+}
